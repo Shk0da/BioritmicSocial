69dbf8bde72c4488e6093ac2573839ce7b89cd85
shkoda@n <shkoda@nottrue.ru>
2015-08-31 11:01:56 +0500

    init

---
+++ app/config/database.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'default' => 'pgsql',
+
+    'connections' => [
+
+        'pgsql' => [
+            'driver'   => 'pgsql',
+            'host'     => 'localhost',
+            'database' => 'bioritmic',
+            'username' => 'postgres',
+            'password' => 'ec0707',
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+        ],
+
+    ],
+
+];
