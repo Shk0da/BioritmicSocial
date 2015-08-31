e7865b7842e92f28cc329e9539c22faaac099e15
shkoda@n <shkoda@nottrue.ru>
2015-08-31 13:19:34 +0500

    checkpoint

---
+++ resources/views/main.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Main</title>
+        <meta name="description" content="Main">
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="http://yastatic.net/bootstrap/3.3.4/css/bootstrap.min.css">
+        <script src="http://yastatic.net/jquery/2.1.4/jquery.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <h1>Welcome to Bioritmic!</h1>
+                    <label>Please Login:</label>
+                    <form>
+                        <div class="form-group">
+                            <label for="email">Email address</label>
+                            <input type="email" class="form-control" id="email" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" id="password" placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-default">Login</button>
+                    </form>
+                    <p>https://www.livecoding.tv/video/social-network-pusher-angular-laravel/</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
